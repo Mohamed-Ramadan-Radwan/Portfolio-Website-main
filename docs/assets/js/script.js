515979f63b3220ed31f2891c1f8d135a864e39b2
Mohamed-Ramadan-Radwan <1.83710491e+08+Mohamed-Ramadan-Radwan@users.noreply.github.com>
2026-08-19 15:48:21 +0000

    Fix homepage project image paths

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -152,7 +152,7 @@ function showProjects(projects) {
     projects.slice(0, 3).forEach(project => {
         projectHTML += `
         <div class="box tilt">
-      <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="project" />
+      <img draggable="false" src="./assets/images/projects/${project.image}" alt="project" />
       <div class="content">
         <div class="tag">
         <h3>${project.name}</h3>
